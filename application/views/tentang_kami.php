4da3f9c2c3d17f778ee5e6c4bbcad79233cf9c88
voidfoxtamcin <voidfoxtamcin@gmail.com>
2024-05-10 10:13:14 +0700

    Added tentang_kami.php File

---
+++ application/views/tentang_kami.php
@@ -0,0 +1,14 @@
+<div class="container py-3">
+  <h1 class="display-4">Tentang Kami</h1>
+  <hr>
+  <div class="row">
+    <div class="col-md-4">
+      <p class="h4 fw-bold">Nama</p>
+      <p class="lead mb-0">Dyodi Ramadhesta Sipayung (51420342)</p>
+      <p class="lead ">Muhammad Anugrah Pratama (50420791)</p>
+      <hr>
+      <p class="h4 fw-bold">Kelas</p>
+      <p class="lead ">4IA02</p>
+    </div>
+  </div>
+</div>
